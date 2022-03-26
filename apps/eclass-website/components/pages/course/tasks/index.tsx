f14ee11476fae8a63e819a17c8cb546dbd06e82c
Alejandro <ale.navarini99@gmail.com>
2022-03-26 16:54:15 -0300

    Tasks: improvements, missing dynamic questions

--- apps/eclass-website/components/pages/course/tasks/index.tsx
+++ apps/eclass-website/components/pages/course/tasks/index.tsx
@@ -1,38 +1,19 @@
-import { Divider, HStack, Text, VStack } from "@chakra-ui/react";
-import { Course, Task } from "@prisma/client";
-import { useCurrentUser } from "../../../../hooks/useCurrentUser";
-import { CardBody } from "../../../Card";
+import { Flex, FormLabel, GridItem, Input, Text } from "@chakra-ui/react";
 
-export const TasksList = ({ course }) => {
-  const me = useCurrentUser();
+export const Questions = ({ questions }) => {
   return (
-    <CardBody px={0} py={0}>
-        <VStack align="left" spacing={0} divider={<Divider />}>
-          {course?.Tasks.map((task, i: number) => (
-            <HStack
-              key={i}
-              py={3}
-              px={4}
-              justify="space-between"
-              as="button"
-              _hover={{
-                backgroundColor: "gray.50",
-              }}
-              sx={{
-                transition: "background-color 0.2s ease-in-out",
-              }}
-              
-            >
-              <HStack spacing="4">
-                <HStack spacing="2">
-                  <Text fontSize="md">
-                    {task.name}
-                  </Text>
-                </HStack>
-              </HStack>
-            </HStack>
-          ))}
-        </VStack>
-      </CardBody>
-  )
+    <>
+      {/* <Text> Se cargo Questions {questions} </Text> */}
+      {questions.forEach((question: Number) => {
+        <>
+          <GridItem colSpan={1} width="100%" justifySelf="end">
+            <FormLabel>Question {question}</FormLabel>
+          </GridItem>
+          <GridItem colSpan={4}>
+            <Input placeholder="Answer to your question(optional)" />
+          </GridItem>
+        </>;
+      })}
+    </>
+  );
 };
--- apps/eclass-website/pages/course/[slug]/tasks.tsx
+++ apps/eclass-website/pages/course/[slug]/tasks.tsx
@@ -1,41 +1,118 @@
 import type { NextPage } from "next";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { prisma } from "../../../lib/prisma";
 import { CourseLayout, courseContext } from "../../../layouts/course-layout";
-import { Avatar, Button, Flex, GridItem, HStack, Text } from "@chakra-ui/react";
-import { Loader } from "../../../components/Loader";
-import { TasksList } from "../../../components/pages/course/tasks";
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  FormControl,
+  FormLabel,
+  Grid,
+  GridItem,
+  HStack,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { Questions } from "../../../components/pages/course/tasks";
 import { Card, CardBody } from "../../../components/Card";
-import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { AddIcon, ArrowDownIcon, ArrowUpIcon } from "@chakra-ui/icons";
 
 const Tasks: NextPage = ({ course }) => {
   const { setCourse } = useContext(courseContext);
+  const [showForm, setShowForm] = useState(false);
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [questions, setQuestions] = useState([0]);
   useEffect(() => {
     setCourse(course);
   }, []);
 
   return (
     <GridItem colSpan={12}>
-      <Flex justify="center" width="100%" height="20rem">
+      <Grid justify="center" width="100%" height="auto">
         <Card>
-          <CardBody>
-          <HStack align="center" justify="space-between">
+          <CardBody width="100%">
+            <HStack align="center" justify="space-between">
               <HStack>
                 <Avatar size="sm" />
                 <Text fontSize="sm">Crea una tarea!</Text>
               </HStack>
-              <Button>
-                <ArrowForwardIcon />
+              <Button onClick={() => setShowForm(!showForm)}>
+                {showForm ? <ArrowUpIcon /> : <ArrowDownIcon />}
               </Button>
             </HStack>
+
+            {showForm && (
+              <>
+                <Divider />
+                <Grid
+                  paddingTop="1.2em"
+                  templateColumns="repeat(5, 1fr)"
+                  width="80%"
+                  align="center"
+                  gap={4}
+                >
+                  <GridItem colSpan={1} alignSelf="end" justifySelf="end">
+                    <FormLabel>Name</FormLabel>
+                  </GridItem>
+                  <GridItem colSpan={4}>
+                    <Input width="100%" placeholder="Task name" />
+                  </GridItem>
+                  <GridItem colSpan={1} alignSelf="end" justifySelf="end">
+                    <FormLabel>Description</FormLabel>
+                  </GridItem>
+                  <GridItem colSpan={4}>
+                    <Input width="100%" placeholder="Task description" />
+                  </GridItem>
+                  {/* TODO: Poner un datetimepicker */}
+                  <GridItem colSpan={1} alignSelf="end" justifySelf="end">
+                    <FormLabel>Start Date</FormLabel>
+                  </GridItem>
+                  <GridItem colSpan={4}>
+                    <Input width="100%" placeholder="dd/MM/yyyy hh:mm" />
+                  </GridItem>
+                  <GridItem colSpan={1} alignSelf="end" justifySelf="end">
+                    <FormLabel>End Date</FormLabel>
+                  </GridItem>
+                  <GridItem colSpan={4}>
+                    <Input width="100%" placeholder="dd/MM/yyyy hh:mm" />
+                  </GridItem>
+                  <GridItem colSpan={5} width="100%">
+                  <Questions
+                    questions={questions}
+                  />
+                  </GridItem>
+                  <GridItem colSpan={1} colStart={2} justifySelf="start">
+                    <Button
+                      onClick={() => {
+                        setQuestionNumber(questionNumber + 1);
+                        if (questions.length === 1 && questions[0] === 0) setQuestions([1]);
+                        else setQuestions(questions.concat([questionNumber]));
+                      }
+                      }
+                    >
+                      <AddIcon />
+                    </Button>
+                  </GridItem>
+
+                  <GridItem colSpan={5} justifySelf="end">
+                    <Button width="100%">Submit</Button>
+                  </GridItem>
+                </Grid>
+              </>
+            )}
           </CardBody>
         </Card>
-        {course.tasks ?
-         <Loader /> : 
-         <TasksList 
-          course={course}
-         />}
-        
+      </Grid>
+      <Flex>
+        {course.tasks ? (
+          <Text> No hay tareas en el curso</Text>
+        ) : (
+          <Text> Tareas </Text>
+        )}
       </Flex>
     </GridItem>
   );
@@ -52,9 +129,9 @@ export const getServerSideProps = async (context: any) => {
     where: {
       slug: context.params.slug,
     },
-    include:{
-      tasks: true
-    }
+    include: {
+      tasks: true,
+    },
   });
   return {
     props: { course },
