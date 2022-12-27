50f2691f538f6c845ea32a5b2f42e21763965dc1
Vittorio Retrivi <vittorioretrivi@gmail.com>
2022-12-27 19:45:42 -0300

    minor

--- apps/eclass-website/components/Footer/index.tsx
+++ apps/eclass-website/components/Footer/index.tsx
@@ -8,9 +8,16 @@ export const Footer = () => {
 
   return (
     <Box as="footer" role="contentinfo" w="100%" mt="auto">
-      <Box bg="gray.100" color="gray.500" mt="8" textAlign="center"    sx={{
-        boxShadow: "0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)",
-      }}>
+      <Box
+        bg="gray.100"
+        color="gray.500"
+        mt="8"
+        textAlign="center"
+        sx={{
+          boxShadow:
+            "0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)",
+        }}
+      >
         <Stack
           maxW="1300"
           px="5"
